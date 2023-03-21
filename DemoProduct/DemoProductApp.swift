437e497c33cbed88e3b8e742e71e45a0d639b8d4
Madhu <madhuadars@gmail.com>
2023-03-21 11:54:00 +0000

    Initial Commit

---
+++ DemoProduct/DemoProductApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoProductApp.swift
+//  DemoProduct
+//
+//  Created by Manoj Pandiyan on 21/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct DemoProductApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
